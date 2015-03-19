1f6f7f958e520b23094a3e59889725c4c654de80
huangjiej <gavin.j.j.huang@gmail.com>
2015-03-19 10:34:35 +0800

    添加公用的状态常量

---
+++ common/src/main/java/com/hummingbird/common/constant/CommonStatusConst.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ * CommonStatusConst.java
+ * 版本所有 深圳市蜂鸟娱乐有限公司 2013-2014
+ */
+package com.hummingbird.common.constant;
+
+/**
+ * @author john huang
+ * 2015年3月18日 下午10:18:07
+ * 本类主要做为通用的状态常量类
+ */
+public abstract class CommonStatusConst {
+
+	/**
+	 * 正常
+	 */
+	public static final String STATUS_OK="OK#"; 
+	
+	/**
+	 * 失败
+	 */
+	public  static final String STATUS_FAIL="FLS";
+	/**
+	 * 冻结
+	 */
+	public  static final String STATUS_FROZEN="FRZ";
+	/**
+	 * 上线
+	 */
+	public  static final String STATUS_ON="ON#";
+	/**
+	 * 下线
+	 */
+	public  static final String STATUS_OFF="OFF";
+	
+	
+}
