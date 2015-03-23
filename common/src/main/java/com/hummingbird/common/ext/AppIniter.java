bbf13792e82e2241cb84fd96f08226a3e775b520
huangjiej <gavin.j.j.huang@gmail.com>
2015-03-23 17:45:58 +0800

    功能新增：添加listener初始化的代码，添加公用常量

---
+++ common/src/main/java/com/hummingbird/common/ext/AppIniter.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * AppIniter.java
+ * 版本所有 深圳市蜂鸟娱乐有限公司 2013-2014
+ */
+package com.hummingbird.common.ext;
+
+import java.util.Map;
+
+/**
+ * @author john huang
+ * 2015年3月21日 下午10:21:00
+ * 本类主要做为应用初始化
+ */
+public interface AppIniter {
+
+	/**
+	 * 初始化
+	 * @param param
+	 */
+	public void init(Map param);
+	
+	
+}
