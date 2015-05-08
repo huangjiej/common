da61e80fd6dca1c9d61b51b2e1b56bfd13aebe7a
huangjiej <gavin.j.j.huang@gmail.com>
2015-05-09 07:00:33 +0800

    增加更多的方法

--- common/src/main/java/com/hummingbird/common/util/PropertiesUtil.java
+++ common/src/main/java/com/hummingbird/common/util/PropertiesUtil.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
+import org.apache.commons.lang.math.NumberUtils;
+
 public class PropertiesUtil {
 	Properties prop = new Properties();
 	
@@ -32,5 +34,39 @@ public class PropertiesUtil {
 	public String getProperty(String key) {
 		
 		return prop.getProperty(key);
+	}
+	
+	/**
+	 * 获取boolean类型
+	 * @param key
+	 * @return
+	 */
+	public boolean getBoolean(String key){
+		String keystr = getProperty(key);
+		return "true".equalsIgnoreCase(keystr)||"1".equalsIgnoreCase(keystr)||"yes".equalsIgnoreCase(keystr);
+	}
+	/**
+	 * 获取boolean类型
+	 * @param key
+	 * @return
+	 */
+	public boolean getBoolean(String key,boolean nulldefault){
+		String keystr = getProperty(key);
+		if(keystr==null)
+		{
+			return nulldefault;
+		}
+		return "true".equalsIgnoreCase(keystr)||"1".equalsIgnoreCase(keystr)||"yes".equalsIgnoreCase(keystr);
+	}
+	
+	/**
+	 * 获取int值
+	 * @param key
+	 * @param nulldefault
+	 * @return
+	 */
+	public int getInt(String key,int nulldefault){
+		String keystr = getProperty(key);
+		return NumberUtils.toInt(keystr,nulldefault);
 	}
 }
