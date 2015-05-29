9f90303db0495ab735bdc886facd880cd2ff6278
huangjiej <gavin.j.j.huang@gmail.com>
2015-05-29 10:40:51 +0800

    调整des加密工具类

--- common/src/main/java/com/hummingbird/common/util/DESUtil.java
+++ common/src/main/java/com/hummingbird/common/util/DESUtil.java
@@ -1,5 +1,6 @@
 package com.hummingbird.common.util;
 
+import java.io.UnsupportedEncodingException;
 import java.security.SecureRandom;
 
 import javax.crypto.Cipher;
@@ -24,6 +25,34 @@ public class DESUtil {
 			// DES算法要求有一个可信任的随机数源
 			SecureRandom sr = new SecureRandom();
 			DESKeySpec deskey = new DESKeySpec(DES_KEY);
+			// 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
+			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+			SecretKey key = keyFactory.generateSecret(deskey);
+			// 加密对象
+			Cipher cipher = Cipher.getInstance("DES");
+			cipher.init(Cipher.ENCRYPT_MODE, key, sr);
+			// 加密，并把字节数组编码成字符串
+			encryptedData = new sun.misc.BASE64Encoder().encode(cipher
+					.doFinal(data.getBytes()));
+		} catch (Exception e) {
+			// log.error("加密错误，错误信息：", e);
+			throw new RuntimeException("加密错误，错误信息：", e);
+		}
+		return encryptedData;
+	}
+	/**
+	 * 数据加密，算法（DES）
+	 * 
+	 * @param data
+	 *            要进行加密的数据
+	 * @return 加密后的数据
+	 */
+	public static String encryptDes(String data,String DESkey) {
+		String encryptedData = null;
+		try {
+			// DES算法要求有一个可信任的随机数源
+			SecureRandom sr = new SecureRandom();
+			DESKeySpec deskey = new DESKeySpec(DESkey.getBytes());
 			// 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
 			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
 			SecretKey key = keyFactory.generateSecret(deskey);
@@ -69,4 +98,40 @@ public class DESUtil {
 		}
 		return decryptedData;
 	}
+	/**
+	 * 数据解密，算法（DES）
+	 * 
+	 * @param cryptData
+	 *            加密数据
+	 * @return 解密后的数据
+	 * @throws UnsupportedEncodingException 
+	 */
+	public static String decodeDES(String cryptData,String DESkey) throws UnsupportedEncodingException {
+		String decryptedData = null;
+		
+		try {
+			// DES算法要求有一个可信任的随机数源
+			SecureRandom sr = new SecureRandom();
+			DESKeySpec deskey = new DESKeySpec(DESkey.getBytes());
+			// 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
+			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+			SecretKey key = keyFactory.generateSecret(deskey);
+			// 解密对象
+			Cipher cipher = Cipher.getInstance("DES");
+			cipher.init(Cipher.DECRYPT_MODE, key, sr);
+			// 把字符串解码为字节数组，并解密
+			decryptedData = new String(
+					cipher.doFinal(new sun.misc.BASE64Decoder()
+							.decodeBuffer(cryptData)));
+		} catch (Exception e) {
+			// log.error("解密错误，错误信息：", e);
+			throw new RuntimeException("解密错误，错误信息：", e);
+		}
+		return decryptedData;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(DESUtil.encryptDes("223344", "12345678"));
+	}
+	
 }
